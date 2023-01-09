bf464c7938d4e136196a440d814af5cb42ba98de
Siddarth Balaji <sid.balaji@berkeley.edu>
2023-01-09 14:46:00 -0500

    completed lab with all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+  function distanceFromHqInBlocks(someValue) {
+    //returns the number of blocks given a value
+    return Math.abs(someValue - 42)
+  }
+
+function distanceFromHqInFeet(someValue){
+    
+    let distInBlocks = distanceFromHqInBlocks(someValue)
+    // based off of indexTest.js it looks like they want 1 block to equal 264 feet
+    return (distInBlocks*264)
+    }
+
+function distanceTravelledInFeet(blockStart, blockEnd){
+    let distInBlocks = Math.abs(blockEnd - blockStart)
+    return (distInBlocks*264)
+    }
+
+function calculatesFarePrice(start, destination){
+    let blocksTraveled = Math.abs(destination - start)
+    let feetTraveled = blocksTraveled*264
+    //first see if the ride is free
+    console.log(blocksTraveled)
+    console.log(feetTraveled)
+    if (feetTraveled >= 2500){
+        return 'cannot travel that far'
+        }
+    else if (feetTraveled >= 2000){
+        return 25
+        }   
+    
+    else if (feetTraveled> 400 && feetTraveled <2000) {
+        let chargedMiles = feetTraveled - 400
+        return (chargedMiles*.02)
+    }
+    else {
+        return 0
+    }
+    
+}
